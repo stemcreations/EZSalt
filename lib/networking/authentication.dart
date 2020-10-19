d0bae2765f7e77a3bf0154dc1ec820e3771a6acb
Wesleycarothers <wesleycarothers@outlook.com>
2020-10-19 14:19:06 -0500

    loading screen indicator done

--- lib/networking/authentication.dart
+++ lib/networking/authentication.dart
@@ -1,36 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
 
 class AuthService {
   UserCredential currentUser;
   FirebaseFirestore _fireStore = FirebaseFirestore.instance;
-  FirebaseAuth _auth = FirebaseAuth.instance;
+  FirebaseAuth auth = FirebaseAuth.instance;
 
   Future signInWithEmailAndPassword(String email, String password) async {
-    //Must initializeAPP with new updates and must use cloud fireStore
-    Firebase.initializeApp();
-    try {
-      currentUser = await _auth.signInWithEmailAndPassword(
+      currentUser = await auth.signInWithEmailAndPassword(
           email: email,
           password: password
       );
-      //catch login exceptions
-    } on FirebaseAuthException catch (e) {
-      if (e.code == 'user-not-found') {
-        print('No user found for that email.');
-      } else if (e.code == 'wrong-password') {
-        print('Wrong password provided for that user.');
-      }
-    }
   }
 
   //TODO add functionality to push a refresh on the sensor via API... I dont know if API currently exists to do this.
   Future<double> getTankLevel() async {
-    if (_auth.currentUser.uid != null){
+    if (auth.currentUser.uid != null){
     final DocumentSnapshot currentUserTankLevel = await _fireStore
-        .collection('users').doc(_auth.currentUser.uid).get();
+        .collection('users').doc(auth.currentUser.uid).get();
     return currentUserTankLevel.get('percent');
     }
     return null;
@@ -38,9 +26,9 @@ class AuthService {
 
   Future<Map> getProfile() async {
     Map profileData;
-    if (_auth.currentUser.uid != null){
+    if (auth.currentUser.uid != null){
       final DocumentSnapshot currentUserDatabaseProfile = await _fireStore
-          .collection('users').doc(_auth.currentUser.uid).get();
+          .collection('users').doc(auth.currentUser.uid).get();
       profileData = currentUserDatabaseProfile.data();
       return profileData;
     }
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -6,6 +6,7 @@ import 'package:flutter/rendering.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:ez_salt/constants.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -17,6 +18,7 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   String email;
   String password;
+  bool _isAsyncCall = false;
 
   @override
   void initState() {
@@ -24,136 +26,153 @@ class _LoginPageState extends State<LoginPage> {
     super.initState();
   }
 
+  void _submit() async {
+    setState(() {
+      _isAsyncCall = true;
+    });
+    await AuthService().signInWithEmailAndPassword(email, password);
+    if(AuthService().auth.currentUser.uid != null){
+      print(AuthService().auth.currentUser.uid);
+      setState(() {
+        _isAsyncCall = false;
+        Navigator.pushNamed(context, '/home');
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey.shade100,
       body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'EZSalt',
-                style: TextStyle(
-                  fontFamily: 'EZSalt',
-                  color: borderAndTextColor,
-                  fontSize: 30.0,
-                  fontWeight: FontWeight.w900,
-                ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              CustomTextField(
-                onChanged: (text){
-                  setState(() {
-                    email = text.trim();
-                  });
-                },
-                text: 'Username',
-                icon: Icon(Icons.person, color: borderAndTextColor,),
-              ),
-              CustomTextField(
-                onChanged: (text){
-                  setState(() {
-                    password = text.trim();
-                  });
-                },
-                obscureText: true,
-                text: 'Password',
-                icon: Icon(Icons.security, color: borderAndTextColor,),
-              ),
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 30),
-                    child: MaterialButton(
-                      onPressed: () {  },  //TODO need to add function to forgot password
-                      child: Text(
-                        'Forgot Password?',
-                        style: TextStyle(color: borderAndTextColor),
-                      ),
-                    ),
+        child: ModalProgressHUD(
+          inAsyncCall: _isAsyncCall,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'EZSalt',
+                  style: TextStyle(
+                    fontFamily: 'EZSalt',
+                    color: borderAndTextColor,
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.w900,
                   ),
-                ],
-              ),
-              SizedBox(
-                width: 230,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                CustomTextField(
+                  onChanged: (text){
+                    setState(() {
+                      email = text.trim();
+                    });
+                  },
+                  text: 'Username',
+                  icon: Icon(Icons.person, color: borderAndTextColor,),
+                ),
+                CustomTextField(
+                  onChanged: (text){
+                    setState(() {
+                      password = text.trim();
+                    });
+                  },
+                  obscureText: true,
+                  text: 'Password',
+                  icon: Icon(Icons.security, color: borderAndTextColor,),
+                ),
+                Row(
                   children: [
-                    ReusableOutlineButton(
-                      size: 110,
-                      label: Text('      Login', style: TextStyle(fontWeight: FontWeight.bold),),
-                      icon: Icon(Icons.arrow_forward, size: 0, ),
-                      onPressed: () async {
-                        await AuthService().signInWithEmailAndPassword(email, password);
-                        var depth = await AuthService().getTankLevel();
-                        print(depth);
-                        Map data = await AuthService().getProfile();
-                        print(data);
-                        Navigator.pushNamed(context, '/home');
-                      },
-                    ),
-                    ReusableOutlineButton(
-                      size: 110,
-                      label: Text('    Register', style: TextStyle(fontWeight: FontWeight.bold),),
-                      icon: Icon(Icons.arrow_forward, size: 0,),
-                      onPressed: (){
-                        Navigator.pushNamed(context, '/register');
-                      },
+                    Padding(
+                      padding: const EdgeInsets.only(left: 30),
+                      child: MaterialButton(
+                        onPressed: () {  },  //TODO need to add function to forgot password
+                        child: Text(
+                          'Forgot Password?',
+                          style: TextStyle(color: borderAndTextColor),
+                        ),
+                      ),
                     ),
                   ],
                 ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Expanded(
-                    child: Container(
-                      child: Divider(
-                        indent: 70,
-                        endIndent: 5,
-                        thickness: 2,
-                        color: borderAndTextColor,
+                SizedBox(
+                  width: 230,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      ReusableOutlineButton(
+                        size: 110,
+                        label: Text('      Login', style: TextStyle(fontWeight: FontWeight.bold),),
+                        icon: Icon(Icons.arrow_forward, size: 0, ),
+                        onPressed: () async {
+                          _submit();
+                          // var depth = await AuthService().getTankLevel();
+                          // print(depth);
+                          // Map data = await AuthService().getProfile();
+                          // print(data);
+                          //
+                        },
+                      ),
+                      ReusableOutlineButton(
+                        size: 110,
+                        label: Text('    Register', style: TextStyle(fontWeight: FontWeight.bold),),
+                        icon: Icon(Icons.arrow_forward, size: 0,),
+                        onPressed: (){
+                          Navigator.pushNamed(context, '/register');
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Expanded(
+                      child: Container(
+                        child: Divider(
+                          indent: 70,
+                          endIndent: 5,
+                          thickness: 2,
+                          color: borderAndTextColor,
+                        ),
                       ),
                     ),
-                  ),
-                  Text(
-                    'OR',
-                    style: TextStyle(color: borderAndTextColor),
-                  ),
-                  Expanded(
-                    child: Container(
-                      child: Divider(
-                        indent: 5,
-                        endIndent: 70,
-                        thickness: 2,
-                        color: borderAndTextColor,
+                    Text(
+                      'OR',
+                      style: TextStyle(color: borderAndTextColor),
+                    ),
+                    Expanded(
+                      child: Container(
+                        child: Divider(
+                          indent: 5,
+                          endIndent: 70,
+                          thickness: 2,
+                          color: borderAndTextColor,
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-              ReusableOutlineButton(
-                size: 230,
-                icon: Icon(FontAwesomeIcons.google),
-                label: Text('   Continue with Google'),
-                onPressed: (){},
-              ),
-              //TODO add facebook login integration with firebase and facebook to allow signin with Facebook
-              // Padding(
-              //   padding: const EdgeInsets.only(bottom: 3),
-              //   child: ReusableOutlineButton(
-              //     size: 230,
-              //     icon: Icon(FontAwesomeIcons.facebook),
-              //     label: Text('   Continue with Facebook'),
-              //     onPressed: (){},
-              //   ),
-              // ),
-            ],
+                  ],
+                ),
+                ReusableOutlineButton(
+                  size: 230,
+                  icon: Icon(FontAwesomeIcons.google),
+                  label: Text('   Continue with Google'),
+                  onPressed: (){},
+                ),
+                //TODO add facebook login integration with firebase and facebook to allow signin with Facebook
+                // Padding(
+                //   padding: const EdgeInsets.only(bottom: 3),
+                //   child: ReusableOutlineButton(
+                //     size: 230,
+                //     icon: Icon(FontAwesomeIcons.facebook),
+                //     label: Text('   Continue with Facebook'),
+                //     onPressed: (){},
+                //   ),
+                // ),
+              ],
+            ),
           ),
         ),
       ),// This trailing comma makes auto-formatting nicer for build methods.
